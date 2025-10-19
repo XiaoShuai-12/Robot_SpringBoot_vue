d598ea8a39f71c400c3a10c90b8dd9deaef6b408
XiaoShuai <2846570993@qq.com>
2025-10-19 18:10:55 +0800

    初始化项目

---
+++ demo_SpringBoot_IDEA/RobotDemo/src/main/java/com/xiaoshuai/robotdemo/RobotDemoApplication.java
@@ -0,0 +1,13 @@
+package com.xiaoshuai.robotdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RobotDemoApplication.class, args);
+    }
+
+}
---
+++ demo_SpringBoot_IDEA/RobotDemo/src/test/java/com/xiaoshuai/robotdemo/RobotDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaoshuai.robotdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobotDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
